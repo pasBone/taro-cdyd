d9827b6b0b0ec4a8b7ec70166438531429707fd5
pasbone <pasbone2@gmail.com>
2019-08-27 17:35:20 +0800

    :recycle: 格式化代码

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,6 @@ import Taro, { Component, Config } from '@tarojs/taro'
 import { Provider } from '@tarojs/mobx'
 import Index from './pages/login/index'
 import counterStore from './store/counter'
-
 import './app.scss'
 
 // 如果需要在 h5 环境中开启 React Devtools
