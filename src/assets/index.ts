4a9883ec8ddf960662f03823f9d6237c33acc5d9
pasbone <pasbone2@gmail.com>
2019-08-27 15:24:10 +0800

    :sparkles: 添加静态资源应用

---
+++ src/assets/index.ts
@@ -0,0 +1,61 @@
+const path = `./images/${THEME_CONFIG.name}`;
+
+export const IMAGE_MAP = {
+    /** 登录页面背景 */
+    loginBg: 'http://wap.succtime.com/img/login_bg@2x.a62fc8e5.png',
+
+    /** 默认头像 */
+    defaultAvatar: require(path + '/default-user.png'),
+    // /** 地图标记图标 */
+    // mapMakerLocation: require(`${path}/icon_location@2x.png`),
+    // /** 地图定位图标 */
+    // mapLocation: require(`${path}/location@2x.png`),
+    /** 登录界面logo */
+    logo: require(path + '/logo@2x.png'),
+    // /** 首页电站详情 */
+    // detailsIcon: require(`${path}/details@2x.png`),
+    // /** 首页电站详情 */
+    // navigation: require(`${path}/navigation@2x.png`),
+    // /** 成功 */
+    // success: require(`${path}/successed@2x.png`),
+    // /** 钱包界面背景 */
+    // walletBg: require(`${path}/wallet_bg@2x.png`),
+    // /** 充电中背景 */
+    // recharingBg: require(`${path}/recharing-bg.png`),
+    // /** 充电中圆环背景 */
+    // calibration: require(`${path}/calibration@2x.png`),
+    // /** 充电中number */
+    // number: require(`./images/number@2x.png`),
+    // /** 充电中voltage */
+    // voltage: require(`./images/voltage@2x.png`),
+    // /** 充电中power */
+    // power: require(`./images/power@2x.png`),
+    // /** 充电中current */
+    // current: require(`./images/current@2x.png`),
+    // /** 充电中wallet_s */
+    // wallet: require(`./images/wallet_s@2x.png`),
+    // /** 充电中electricity */
+    // electricity: require(`./images/electricity@2x.png`),
+    // /** code */
+    // code: require(`./images/code@2x.png`),
+    // /** gps */
+    // gps: require(`./images/gps@2x.png`),
+    // /** list */
+    // list: require(`./images/list@2x.png`),
+    // /** map */
+    // map: require(`./images/map@2x.png`),
+    // /** mine */
+    // mine: require(`./images/mine@2x.png`),
+    // /** nodata */
+    // nodata: require(`./images/nodata@2x.png`),
+    // /** order */
+    // order: require(`./images/order@2x.png`),
+    // /** record */
+    // record: require(`./images/record@2x.png`),
+    // /** wallet */
+    // walletIcon: require(`./images/wallet@2x.png`),
+    // /** setting */
+    // setting: require(`./images/setting@2x.png`),
+    // /** vihiclelicense */
+    // vihiclelicense: require(`./images/vihiclelicense.jpg`),
+}
