a1b65db05299574426d0cddd0bb3e79043864e40
pasbone <pasbone2@gmail.com>
2019-08-27 17:34:53 +0800

    :rocket: 添加登录界面功能

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,8 @@
 import { ComponentType } from 'react'
 import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Image } from '@tarojs/components'
+import { View, Image, Text } from '@tarojs/components'
 import { IMAGE_MAP } from '@/assets'
-import { AtForm, AtInput } from 'taro-ui';
+import { AtButton, AtInput } from 'taro-ui';
 import { mebApi } from '@/api/meb';
 import { DEFAULT_REG_WAY } from '@/constant';
 import './index.scss'
@@ -37,31 +37,56 @@ class LoginView extends Component {
 
   componentDidHide() { }
 
-  onTelChange: () => {
+  onTelChange = (tel: string) => {
+    this.setState({
+      tel
+    })
+  }
 
+  onCodeChange = (code: string) => {
+    this.setState({
+      code
+    })
   }
 
   render() {
     // const { counterStore: { counter } } = this.props
     return (
-      <View className='loginView' style={'background-image:url(' + IMAGE_MAP.loginBg + ')'}>
+      <View className='login-view' style={'background-image:url(' + IMAGE_MAP.loginBg + ')'}>
         <View className="logo">
           {/* <Image className="logo-img" src={IMAGE_MAP.logo} /> */}
           <Image className="logo-img" src='' />
         </View>
 
-        <AtForm>
-          <AtInput
-            clear
-            name='tel'
-            type='text'
-            maxLength='4'
-            placeholder='验证码'
-            value={this.state.code}
-            onChange={this.onTelChange}
-          />
+        <View className="login-box">
+          <View className="login-row">
+            <AtInput
+              className="login-input"
+              name='tel'
+              type='phone'
+              border={false}
+              placeholder='请输入手机号码'
+              value={this.state.tel}
+              onChange={this.onTelChange}
+            />
+            <Text className="send-code"> 发送验证码 </Text>
+          </View>
 
-        </AtForm>
+          <View className="login-row">
+            <AtInput
+              className="login-input"
+              name='code'
+              type='number'
+              border={false}
+              placeholder='验证码'
+              maxLength='6'
+              value={this.state.code}
+              onChange={this.onCodeChange}
+            />
+          </View>
+        </View>
+
+        <AtButton className="login-btn" type='primary'>登录</AtButton>
       </View>
     )
   }
