36acb926a79fb251d9b7fe592c80a2e74b204981
pasbone <pasbone2@gmail.com>
2019-09-02 17:18:44 +0800

    :fire 更改请求方式为taro内置模块

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,27 +1,22 @@
-import qs from "query-string"
-import { Observable, of, throwError } from "rxjs"
-import { fromFetch } from "rxjs/fetch"
+import { Observable, of, throwError, from } from "rxjs"
 import { catchError, filter, switchMap, tap } from "rxjs/operators"
 import { OPERATE_CODE, Res } from "@/types"
 import { OPERATOR_CODE } from '@/constant';
+import Taro from '@tarojs/taro';
 
+const BASE_URL = "https://wx.succtime.com/wx"
 
-const BASE_URL = "http://wap.succtime.com/wap"
+let token = "";
 
-const stringify = (obj?: any) => qs.stringify({ ...obj, operator_code: OPERATOR_CODE }, { arrayFormat: "comma" });
-
-const defaultHeaders: RequestInit["headers"] = {
-    "Content-Type": "application/x-www-form-urlencoded",
-    Connection: "close",
-}
-
-let _token = "";
-
-export const setToken = (t: string) => (_token = t)
+export const setToken = (t: string) => (token = t)
 
 enum RequestType {
     "请求数据",
     "上传文件",
+}
+
+const defaultHeaders: RequestInit["headers"] = {
+    "Content-Type": "application/x-www-form-urlencoded",
 }
 
 const genGetHeaderMethods = (type: RequestType) => {
@@ -31,41 +26,40 @@ const genGetHeaderMethods = (type: RequestType) => {
         })
     }
     return () => {
-        return { ...defaultHeaders, token: _token }
+        return { ...defaultHeaders, token }
     }
 }
-const genGetBodyMethods = (type: RequestType) => {
-    if (type === RequestType.上传文件) {
-        return <T>(params: T) => {
-            return params
-        }
-    }
-    return stringify
-}
+
 
 const genReqestMethods = (type: RequestType) => {
     const genHeader = genGetHeaderMethods(type)
-    const genBody = genGetBodyMethods(type)
 
     return <Q = void, P = void>(url: string) => {
         return (params?: Q): Observable<Res<P>> => {
-            return fromFetch(`${BASE_URL}${url}`, {
+
+            const $request = Taro.request({
+                url: `${BASE_URL}${url}`,
                 method: "POST",
-                headers: genHeader(),
-                body: genBody(params),
-            }).pipe(
+                data: {
+                    ...params,
+                    operator_code: OPERATOR_CODE
+                },
+                header: genHeader()
+            });
+
+            return from($request).pipe(
                 switchMap(response => {
-                    if (response.ok) {
-                        return response.json() as Promise<Res<P>>
+                    if (response.statusCode === 200) {
+                        return response.data as Promise<Res<P>>
                     }
                     return throwError({
                         success: false,
-                        message: `Error ${response.status}`,
+                        message: `Error ${response.statusCode}`,
                     })
                 }),
                 tap(res => {
                     if (res.code === OPERATE_CODE.登录信息失效) {
-
+                        console.log(res, '登录信息失效');
                     }
                 }),
                 filter(res => res.code !== OPERATE_CODE.登录信息失效),
