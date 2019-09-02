31842b6fa0bec94631e961c21410da74c55e78b5
pasbone <pasbone2@gmail.com>
2019-09-02 17:20:10 +0800

    :tada 添加todos demo

---
+++ src/store/module/todos/epics.ts
@@ -0,0 +1,42 @@
+import { Epic } from 'redux-observable';
+import { from, of } from 'rxjs';
+import { filter, switchMap, map, catchError } from 'rxjs/operators';
+import { RootAction, RootState, isActionOf, Services } from 'typesafe-actions';
+
+import { loadTodosAsync, saveTodosAsync } from './actions';
+import { getTodos } from './selectors';
+
+export const loadTodosEpic: Epic<
+    RootAction,
+    RootAction,
+    RootState,
+    Services
+> = (action$, state$, { api }) => {
+
+    console.log(api);
+    return action$.pipe(
+        filter(isActionOf(loadTodosAsync.request)),
+        switchMap(() =>
+            from(api.loadSnapshot()).pipe(
+                map(loadTodosAsync.success),
+                catchError((message: string) => of(loadTodosAsync.failure(message)))
+            )
+        )
+    );
+}
+
+export const saveTodosEpic: Epic<
+  RootAction,
+  RootAction,
+  RootState,
+  Services
+> = (action$, state$, { api }) =>
+  action$.pipe(
+    filter(isActionOf(saveTodosAsync.request)),
+    switchMap(() =>
+      from(api.todos.saveSnapshot(getTodos(state$.value.rootReducer.todosReducer))).pipe(
+        map(saveTodosAsync.success),
+        catchError((message: string) => of(saveTodosAsync.failure(message)))
+      )
+    )
+  );
---
+++ src/store/module/todos/types.d.ts
@@ -0,0 +1,7 @@
+declare module 'MyModels' {
+    export type Todo = {
+      id: any;
+      title: string;
+    };
+  }
+  
