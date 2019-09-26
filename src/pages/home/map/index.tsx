f88600cad1c701f5b126b86852d028b782792138
pasbone <pasbone2@gmail.com>
2019-09-26 20:46:03 +0800

    :tada: 添加map

---
+++ src/pages/home/map/index.tsx
@@ -0,0 +1,25 @@
+import { View, Map } from '@tarojs/components'
+import { FC } from '@tarojs/taro'
+import Taro from '@tarojs/taro'
+import './index.scss';
+
+const MapView: FC = () => {
+
+
+    Taro.getLocation({
+        type: 'wgs84',
+        success(res) {
+            console.log(res);
+        }
+    }).then(data => {
+        console.log(data);
+    })
+
+    return (
+        <View>
+            <Map className="map-view" latitude={22.53332} longitude={113.93041} />
+        </View>
+    )
+}
+
+export default MapView;
