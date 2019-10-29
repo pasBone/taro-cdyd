7c1aab06fa6c40e97af8e16bc6f7dd0c9501b515
pasbone <pasbone2@gmail.com>
2019-10-29 10:11:28 +0800

    :sparkles:添加react-demo

---
+++ src/pages/react-typescript/index.tsx
@@ -0,0 +1,38 @@
+import Taro, { FC } from '@tarojs/taro'
+import React from 'react'
+
+/** * 声明注入的Props */
+export interface ThemeProps {
+    primary: string;
+    secondary: string;
+}
+
+/** 给指定组件注入'主题' */
+export function withTheme<P>(Component: React.ComponentType<P & ThemeProps>) {
+    /** WithTheme 自己暴露的Props */
+    interface OwnProps { }
+
+    /** * 高阶组件的props, 忽略ThemeProps, 外部不需要传递这些属性 */
+    type WithThemeProps = P & OwnProps;
+
+    /** * 高阶组件 */
+    const WithTheme = (props: WithThemeProps) => {
+        // 假设theme从context中获取
+        const fakeTheme: ThemeProps = {
+            primary: 'red',
+            secondary: 'blue',
+        };
+        return <Component {...fakeTheme} {...props} />;
+    };
+
+    // WithTheme.displayName = `withTheme${Component.displayName}`;
+
+    return WithTheme;
+}
+
+// Test
+const Foo: FC<{ a: number } & ThemeProps> = props => <div style={{ color: props.primary }} />;
+const FooWithTheme = withTheme(Foo);
+
+export default <FooWithTheme a={1} />;
+
