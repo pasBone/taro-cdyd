f65beaee669dd94d8414c02983b921df36324083
pasbone <pasbone2@gmail.com>
2019-11-05 16:52:42 +0800

    :art: 优化home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,17 +4,22 @@ import { IMAGE_MAP } from "@/assets"
 import { APP_NAME } from "@/constant"
 import { View, Image } from "@tarojs/components"
 import Navbar from '@/components/layout/nav-bar'
-import { useSetSideMenuState } from '@/hooks/use-set-side-menu-state'
+import SideMenu from '@/components/layout/side-menu'
+import { useDispatch, useSelector } from "@tarojs/redux";
+import { setSideMenuClose, setSideMenuOpen } from '@/store/module/common/common.actions'
+import { RootState } from '@/store/types'
 
 const HomeView: FC = () => {
+    const dispatch = useDispatch();
+    const menuState = useSelector((state: RootState) => state.common.sideMenu.state);
     return (
-        <View>
+        <View className="home__view">
             <Navbar
-                title="首页"
-                renderLeftIcon={<Image src={IMAGE_MAP.mine} onClick={() => useSetSideMenuState()} />}
+                title='首页'
+                renderLeftIcon={<Image src={IMAGE_MAP.mine} onClick={() => dispatch(menuState ? setSideMenuClose() : setSideMenuOpen())} />}
                 renderRightIcon={<Image src={IMAGE_MAP.list} />}
             />
-
+            <SideMenu />
             <MapView />
         </View>
     )
