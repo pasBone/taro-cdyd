aff5a187c916013d821dccece639fb008f356be2
pasbone <pasbone2@gmail.com>
2019-11-06 16:23:00 +0800

    :todo: 地图定位不成功

--- src/pages/home/map/index.tsx
+++ src/pages/home/map/index.tsx
@@ -1,21 +1,34 @@
-import { View, Map } from '@tarojs/components'
-import Taro, { FC } from '@tarojs/taro'
+import { View, Map, CoverImage } from '@tarojs/components'
+import Taro, { FC, useCallback, useEffect, createMapContext, useState, useRef } from '@tarojs/taro'
+import { IMAGE_MAP } from '@/assets';
+import { useSelector, useDispatch } from '@tarojs/redux';
+import { RootState } from '@/store/types';
+import { getLocationAsync } from '@/store/module/common/common.actions'
 import './index.scss';
 
 const MapView: FC = () => {
+    const dispatch = useDispatch();
+    const { latitude, longitude } = useSelector((state: RootState) => state.common.gpsLocation);
+    const mapRef = useRef<any>();
 
-    Taro.getLocation({
-        type: 'wgs84',
-        success(res) {
-            // console.log(res);
-        }
-    }).then(
-        // console.log
-    )
+    useEffect(() => {
+        mapRef.current = createMapContext('homeMap')
+    }, [])
+
+    const getLocation = useCallback(() => {
+        dispatch(getLocationAsync(true));
+        mapRef.current.moveToLocation();
+    }, [latitude, longitude])
 
     return (
-        <View>
-            <Map className="map-view" latitude={22.53332} longitude={113.93041} />
+        <View className="home-map__view">
+            <CoverImage onClick={getLocation} className="gps-icon" src={IMAGE_MAP.gps} />
+            <Map
+                id="homeMap"
+                latitude={latitude}
+                longitude={longitude}
+                show-location
+            />
         </View>
     )
 }
