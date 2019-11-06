184ba4acb2913c414b3b7473d0613af0b9dfee67
pasbone <pasbone2@gmail.com>
2019-11-06 17:21:59 +0800

    :art: 拆分地图组件

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,21 @@
 import MapView from './map'
-import { FC } from "@tarojs/taro"
+import { FC, createMapContext } from "@tarojs/taro"
 import { IMAGE_MAP } from "@/assets"
 import { APP_NAME } from "@/constant"
-import { View, Image } from "@tarojs/components"
+import { View, Image, Map } from "@tarojs/components"
 import Navbar from '@/components/layout/nav-bar'
 import SideMenu from '@/components/layout/side-menu'
+import ChargeEntry from './charge-entry';
 import { useDispatch, useSelector } from "@tarojs/redux";
 import { setSideMenuClose, setSideMenuOpen } from '@/store/module/common/common.actions'
 import { RootState } from '@/store/types'
 
+
 const HomeView: FC = () => {
     const dispatch = useDispatch();
     const menuState = useSelector((state: RootState) => state.common.sideMenu.state);
+    const { latitude, longitude } = useSelector((state: RootState) => state.common.gpsLocation);
+    
     return (
         <View className="home__view">
             <Navbar
@@ -20,7 +24,15 @@ const HomeView: FC = () => {
                 renderRightIcon={<Image src={IMAGE_MAP.list} />}
             />
             <SideMenu />
-            <MapView />
+
+            {false && <ChargeEntry />}
+
+            {/* 小程序组件有坑，直接拆分组件无法拿到map实例，采用props传递 */}
+            <MapView
+                mapCtx={createMapContext('homeMap')}
+                renderMapView={<Map id="homeMap" latitude={latitude} longitude={longitude} show-location={true} />}
+            />
+
         </View>
     )
 }
