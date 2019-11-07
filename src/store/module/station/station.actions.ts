43442ec7c1a05907cd80357b6e357f6f5c586341
pasbone <pasbone2@gmail.com>
2019-11-07 18:00:50 +0800

    :sparkles: 添加站点详情store

--- src/store/module/station/station.actions.ts
+++ src/store/module/station/station.actions.ts
@@ -3,22 +3,41 @@ import { Dispatch } from 'redux'
 import { stationApi } from '@/api/station'
 
 
+/** 获取站点列表 */
 export const getStationListAsync = (params?: stationApi.ListReq) => {
-
   return async (dispatch: Dispatch) => {
     try {
-      dispatch(
-        getStationList()
-      )
+      dispatch(getStationList());
       const response = await stationApi.list(params);
-      dispatch(
-        getStationSuccess(response.data)
-      )
-
+      dispatch(getStationSuccess(response.data));
     } catch (error) { }
   }
 }
 
+/** 获取站点详情 */
+export const getStationDetailsAsync = (params: stationApi.DetailReq) => {
+  return async (dispatch: Dispatch) => {
+    try {
+      dispatch(getStationDetails());
+      const response = await stationApi.details(params);
+      dispatch(getStationDetailsSuccess(response.data))
+    } catch (error) { }
+  }
+}
+
+
 /** 获取站点列表 */
-export const getStationList = () => ({ type: types.GET_STATION_LIST })
-export const getStationSuccess = (payload) => ({ type: types.GET_STATION_LIST_SUCCESS, payload })
+export const getStationList = () => ({ type: types.GET_STATION_LIST });
+export const getStationSuccess = (payload) => ({ type: types.GET_STATION_LIST_SUCCESS, payload });
+
+/** 获取站点详情 */
+export const getStationDetails = () => ({ type: types.GET_STATION_DETAILS });
+export const getStationDetailsSuccess = (payload) => ({ type: types.GET_STATION_DETAILS_SUCCESS, payload });
+
+
+export const asyncAction = () => {
+  return (dispatch: Dispatch) => {
+    dispatch({type: 'SOME_SYNC_ACTION'})
+    return Promise.resolve();
+  }
+}
--- src/store/module/station/station.reducer.ts
+++ src/store/module/station/station.reducer.ts
@@ -2,6 +2,7 @@ import { IAction } from "@/store/types";
 import { combineReducers } from "redux";
 import { stationApi } from "@/api/station";
 import * as types from './station.types'
+import { LoadingType } from "@/types";
 
 /** 站点列表 */
 export const stationListState: stationApi.ListRes = {
@@ -33,6 +34,42 @@ export const stationList = (state = stationListState, action: IAction<stationApi
   }
 }
 
+/** 站点详情 */
+export const stationDetailsState: stationApi.DetailRes & LoadingType = {
+  station_id: '',
+  operator_id: '',
+  owner_id: '',
+  station_code: '',
+  station_name: '',
+  station_type: '',
+  address: '',
+  longitude: '',
+  latitude: '',
+  quantity: '',
+  transfor_type: '',
+  max_power: '',
+  station_logo: '',
+  desc: '',
+  distance: 0,
+  fast_fill_all: 0,
+  fast_fill_available: 0,
+  slow_fill_all: 0,
+  slow_fill_available: 0,
+  loading: false,
+  station_status: 1
+}
+export const stationDetails = (state = stationDetailsState, action: IAction<stationApi.DetailRes>) => {
+  switch (action.type) {
+    case types.GET_STATION_DETAILS:
+      return { ...state, loading: true }
+    case types.GET_STATION_DETAILS_SUCCESS:
+      return { ...action.payload, loading: false }
+    default:
+      return state
+  }
+}
+
 export const stationReducer = combineReducers({
-  stationList
+  stationList,
+  stationDetails
 })
