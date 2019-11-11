109c74c2a7f87e26d981cab594f50f4b130e36ca
pasbone <pasbone2@gmail.com>
2019-11-11 11:25:37 +0800

    :tada: 添加电桩列表组件

---
+++ src/pages/station/details/components/pile-list/index.tsx
@@ -0,0 +1,12 @@
+import { View } from "@tarojs/components"
+import { CardBox } from "@/components/card"
+
+
+export const PileList = () => {
+
+  return (
+    <CardBox title="电桩实时状态">
+        <View></View>
+      </CardBox>
+  )
+}
