2b2f06e549d9ec9f9970360f10e77a44923bb3e7
pasbone <pasbone2@gmail.com>
2019-11-12 11:38:02 +0800

    :art: 优化电桩扫码

--- src/store/module/common/common.actions.ts
+++ src/store/module/common/common.actions.ts
@@ -1,7 +1,7 @@
 
 import * as types from './common.types';
 import { Dispatch } from 'redux';
-import { getLocation, scanCode } from '@tarojs/taro'
+import { getLocation, scanCode, navigateTo } from '@tarojs/taro'
 import { reAuthorize } from '@/utils/common';
 import { wxApiError, WX_API_ERROR } from '@/types';
 import Toast from '@/utils/toast';
@@ -42,12 +42,13 @@ export const setLocation = ((payload) => ({ type: types.SET_LOCATION, payload })
 export const scanCodeWithPileAsync = () => {
   return async () => {
     try {
-      const result = await scanCode();
-      console.log(result);
-      // todo code.result
+      const code = await scanCode();
+      navigateTo({
+        url: `/pages/pile/details/index?id=${code.result}`
+      });
     } catch (err) {
       if (err.errMsg !== WX_API_ERROR["扫码-用户取消扫码"]) {
-        Toast.failed('请检测二维码是否正确...')
+        Toast.info('请检测二维码是否正确...')
       }
     }
   }
