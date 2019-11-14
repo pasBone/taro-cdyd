8bc6346d172d663c100e73d842ed34ad9757ba78
pasbone <pasbone2@gmail.com>
2019-11-14 20:12:27 +0800

    :sparkles: 添加cell 组件

---
+++ src/components/list-cell/index.tsx
@@ -0,0 +1,31 @@
+import './style.scss';
+import { FC } from "@tarojs/taro";
+import { View } from "@tarojs/components";
+import { AtIcon } from 'taro-ui';
+
+type IProps = {
+  /** label */
+  label: string,
+  /** value */
+  value?: string,
+  /** value jsx  element */
+  renderValue?: JSX.Element,
+  /** 是否显示箭头 */
+  showArrow?: boolean
+}
+
+export const ListCell: FC<IProps> = (props) => {
+  return (
+    <View className="list-cell__view">
+      <View className="list-cell__item">
+        <View className="label">{props.label}</View>
+        {<View className="value">{props.value || props.renderValue}</View>}
+        {props.showArrow && <View className="arrow-icon"><AtIcon value='chevron-right' size='16' color='#ccc'></AtIcon></View>}
+      </View>
+    </View>
+  )
+}
+
+ListCell.defaultProps = {
+  showArrow: true
+}
