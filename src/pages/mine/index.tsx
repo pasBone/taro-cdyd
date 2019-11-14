ca18ec137cabd9e517c4e44776fefd40484a2452
pasbone <pasbone2@gmail.com>
2019-11-14 20:00:49 +0800

    :sparkles: 增加mine组件

--- src/pages/mine/index.tsx
+++ src/pages/mine/index.tsx
@@ -1,9 +1,20 @@
+import './style.scss'
 import { View } from "@tarojs/components"
+import { FC } from "@tarojs/taro"
+import { useSelector } from "@tarojs/redux"
+import { RootState } from "@/store/types"
+import { ListCell } from '@/components/list-cell'
+import { AtAvatar } from "taro-ui"
 
-export const Mine = () => {
+export const MineView: FC = () => {
   return (
-    <View>
-      mine
+    <View className="mine-view">
+
+      <ListCell label="头像" value="dfsd" renderValue={<View>ddd</View>} showArrow />
+      {/* <ListCell label="手机号码" value="订单" showArrow />
+      <ListCell label="车主认证" value="订单" showArrow />
+      <ListCell label="用户协议" value="订单" showArrow /> */}
+
     </View>
   )
 }
