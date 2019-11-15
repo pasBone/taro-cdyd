5b879ac1dbb44cd5faf47858ac643ce6dd4ddc73
pasbone <pasbone2@gmail.com>
2019-11-15 16:42:32 +0800

    :art: 格式化diamagnetic

--- src/pages/mine/index.tsx
+++ src/pages/mine/index.tsx
@@ -1,5 +1,5 @@
 import './style.scss'
-import { View } from "@tarojs/components"
+import { View, Image } from "@tarojs/components"
 import { FC } from "@tarojs/taro"
 import { useSelector } from "@tarojs/redux"
 import { RootState } from "@/store/types"
@@ -10,10 +10,10 @@ export const MineView: FC = () => {
   return (
     <View className="mine-view">
 
-      <ListCell label="头像" value="dfsd" renderValue={<View>ddd</View>} showArrow />
-      {/* <ListCell label="手机号码" value="订单" showArrow />
+      <ListCell label="头像" renderValue={<Image className="avatar" src='https://jdc.jd.com/img/200' />} showArrow />
+      <ListCell label="手机号码" value="订单" showArrow />
       <ListCell label="车主认证" value="订单" showArrow />
-      <ListCell label="用户协议" value="订单" showArrow /> */}
+      <ListCell label="用户协议" value="订单" showArrow />
 
     </View>
   )
