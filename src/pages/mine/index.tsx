5fb59ac6ea0a64968e890c5eb85a8a27ac0a31d2
pasbone <pasbone2@gmail.com>
2019-11-15 17:50:53 +0800

    :sparkles: 完善组件

--- src/pages/mine/index.tsx
+++ src/pages/mine/index.tsx
@@ -1,20 +1,22 @@
 import './style.scss'
-import { View, Image } from "@tarojs/components"
-import { FC } from "@tarojs/taro"
+import { View, Image, OpenData } from "@tarojs/components"
+import { FC, navigateTo } from "@tarojs/taro"
 import { useSelector } from "@tarojs/redux"
 import { RootState } from "@/store/types"
 import { ListCell } from '@/components/list-cell'
-import { AtAvatar } from "taro-ui"
+import { AtButton } from "taro-ui"
 
 export const MineView: FC = () => {
+  const userProtocol = 'https://wx.youdaocharge.com/#/mine/user-protocol';
   return (
     <View className="mine-view">
 
-      <ListCell label="头像" renderValue={<Image className="avatar" src='https://jdc.jd.com/img/200' />} showArrow />
+      {/* <ListCell label="头像" renderValue={<OpenData className="avatar" type="userAvatarUrl" />} showArrow />
       <ListCell label="手机号码" value="订单" showArrow />
       <ListCell label="车主认证" value="订单" showArrow />
-      <ListCell label="用户协议" value="订单" showArrow />
+      <ListCell label="用户协议" onClick={() => navigateTo({ url: `/pages/webview/index?url=${userProtocol}` })} showArrow /> */}
 
+      <AtButton formType="submit" open-type="getPhoneNumber" onGetPhoneNumber={() => { console.log(1) }} type='primary'>按钮文案</AtButton>
     </View>
   )
 }
