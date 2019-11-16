8cfe91a36092f101766118de8d9df38fcc7255e3
pasbone <pasbone2@gmail.com>
2019-11-16 11:22:47 +0800

    :tada: 添加order api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,13 @@
 import { mebApi } from "./meb"
 import { pileApi } from './pile'
 import { stationApi } from './station'
+import { orderApi } from './order'
 import * as todosApi from './todos'
 
 export const api = {
   meb: mebApi,
   todo: todosApi,
   pile: pileApi,
-  station: stationApi
+  station: stationApi,
+  order: orderApi
 }
