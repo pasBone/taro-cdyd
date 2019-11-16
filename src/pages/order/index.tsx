a421acaa2134956fb9e33c6a4bd3b5a8b6ee077f
pasbone <pasbone2@gmail.com>
2019-11-16 11:10:55 +0800

    :tada: :tada: :tada:  添加订单列表

--- src/pages/order/index.tsx
+++ src/pages/order/index.tsx
@@ -1,9 +1,21 @@
-import { View } from "@tarojs/components"
+import './style.scss'
+import { View, Text } from "@tarojs/components"
 
-export const Order = () => {
+export const OrderListView = () => {
   return (
-    <View>
-      order
+    <View className="order-list__view">
+      <View className="order-list">
+        <View className="order-list__item">
+          <View className="order-list__title">
+            <View className="desc">item.way_desc</View>
+            <Text>￥item.total_fee</Text>
+          </View>
+          <View className="order-list__time">
+            <Text>formatDate(item.paid_time)</Text>
+            <Text>item.recharge_label</Text>
+          </View>
+        </View>
+      </View>
     </View>
   )
 }
