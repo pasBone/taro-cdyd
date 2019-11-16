a9c610f3d8aece04736c70f3c35d11e29abcb9e7
pasbone <pasbone2@gmail.com>
2019-11-16 17:45:53 +0800

    :sparkles: 静态资源管理

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -20,23 +20,21 @@ export const IMAGE_MAP = {
     // success: require(`${path}/successed@2x.png`),
     // /** 钱包界面背景 */
     // walletBg: require(`${path}/wallet_bg@2x.png`),
-    // /** 充电中背景 */
-    // recharingBg: require(`${path}/recharing-bg.png`),
-    // /** 充电中圆环背景 */
+    /** 充电中圆环背景 */
     // calibration: require(`${path}/calibration@2x.png`),
-    // /** 充电中number */
-    // number: require(`./images/number@2x.png`),
-    // /** 充电中voltage */
-    // voltage: require(`./images/voltage@2x.png`),
-    // /** 充电中power */
-    // power: require(`./images/power@2x.png`),
-    // /** 充电中current */
-    // current: require(`./images/current@2x.png`),
-    // /** 充电中wallet_s */
-    // wallet: require(`./images/wallet_s@2x.png`),
-    // /** 充电中electricity */
-    // electricity: require(`./images/electricity@2x.png`),
-    // /** code */
+    /** 充电中number */
+    number: require('./images/number@2x.png'),
+    /** 充电中voltage */
+    voltage: require('./images/voltage@2x.png'),
+    /** 充电中power */
+    power: require('./images/power@2x.png'),
+    /** 充电中current */
+    current: require('./images/current@2x.png'),
+    /** 充电中wallet_s */
+    wallet: require('./images/wallet_s@2x.png'),
+    /** 充电中electricity */
+    electricity: require('./images/electricity@2x.png'),
+    /** code */
     code: require('./images/code@2x.png'),
     /** gps */
     gps: require('./images/gps@2x.png'),
