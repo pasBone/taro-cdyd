201eb277ce52af6617502396c12b09b2b46bc1fb
pasbone <pasbone2@gmail.com>
2019-11-18 19:31:54 +0800

    :fire: 删除文件

--- src/store/root-action.ts
+++
@@ -1,6 +0,0 @@
-// todo删除无用文件
-import * as loginActions from "./module/meb/meb.actions";
-
-export const RootAction = {
-  login: loginActions,
-}
--- src/store/root-services.ts
+++
@@ -1,6 +0,0 @@
-// todo删除无用文件
-import { api } from "@/api"
-
-export const RootServices = {
-  api
-}
