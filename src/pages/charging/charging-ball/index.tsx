973ab85eee5805abf7d166a4fa6db4b1b94fdd18
pasbone <pasbone2@gmail.com>
2019-11-19 15:16:20 +0800

    :sparkles: 完善实时计费功能

--- src/pages/charging/charging-ball/index.tsx
+++ src/pages/charging/charging-ball/index.tsx
@@ -1,32 +1,20 @@
 import './style.scss';
 import { View } from '@tarojs/components';
 import { getDuration } from '@/utils/common';
+import CircleProgress from '@/components/circle-progress';
 
 export const ChargingBall = () => {
-    return (
-        <View className="charging-ball">
-            {/* <md-progress
-                    value={this.chargingProcess / 100}
-                    width={15}
-                    size={210}
-                    rotate={-90}
-                    color="url(#linear)"
-                    border-color="#5996ec"
-                    linecap="butt"
-                    transition={true}
-                > */}
-            <View className="progress-ball">
-                <View className="soc">SOC</View>
-                <View className="value">{60}%</View>
-                <View className="full-time">充满预计{getDuration(25685555, '小时', '分钟', '分')}</View>
-                {/* <View className="full-time">充满预计1小时15分</View> */}
-            </View>
-            {/* <defs slot="defs">
-                        <pattern id="linear" patternUnits="userSpaceOnUse" width="250" height="250">
-                            <image href={this.calibrationBg} x="0" y="0" width="100%" height="100%"></image>
-                        </pattern>
-                    </defs> */}
-            {/* </md-progress> */}
-        </View>
-    )
+  return (
+    <View className="charging-ball">
+      <View className="progress-ball">
+        <CircleProgress>
+          <View className="progress-content">
+            <View className="soc">SOC</View>
+            <View className="value">{60}%</View>
+            <View className="full-time">充满预计{getDuration(25685555, '小时', '分钟', '分')}</View>
+          </View>
+        </CircleProgress>
+      </View>
+    </View>
+  )
 }
