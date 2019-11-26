77c9c2d12073d132934b7bb9898a9b0f4be8d601
pasbone <pasbone2@gmail.com>
2019-11-26 10:17:45 +0800

    :art: 修改api地址

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -3,8 +3,8 @@ import { OPERATOR_CODE } from '@/constant';
 import Taro, { getStorageSync, navigateTo } from '@tarojs/taro';
 import Toast from './toast';
 
-export const BASE_URL = 'https://wx.succtime.com/wx';
-// const BASE_URL = "https://wx.youdaocharge.com/wx"
+// export const BASE_URL = 'https://wx.succtime.com/wx';
+const BASE_URL = "https://wx.youdaocharge.com/wx"
 
 enum RequestType {
 	'请求数据',
